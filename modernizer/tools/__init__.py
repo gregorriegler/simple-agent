b254d5b1484fbe344d2c8d767937887f3ef25bc3
Gregor Riegler <rieglerg85@gmail.com>
2025-06-15 20:55:20 +0200

    r

--- modernizer/tools/__init__.py
+++ modernizer/tools/__init__.py
@@ -1,8 +1,6 @@
 from .base_tool import BaseTool
 from .tool_library import ToolLibrary
 from .cat_tool import CatTool
-from .extract_method_tool import ExtractMethodTool
-from .inline_method_tool import InlineMethodTool
 from .ls_tool import LsTool
 from .revert_tool import RevertTool
 from .test_tool import TestTool
@@ -11,8 +9,6 @@ __all__ = [
     'BaseTool',
     'ToolLibrary',
     'CatTool',
-    'ExtractMethodTool',
-    'InlineMethodTool',
     'LsTool',
     'RevertTool',
     'TestTool'
