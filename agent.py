eeeb6db5d06faa2d78c20ecc5eece4a208788758
Gregor Riegler <rieglerg85@gmail.com>
2025-08-29 17:38:00 +0200

    f wait for escape

--- agent.py
+++ agent.py
@@ -1,5 +1,6 @@
 from chat import save_chat
 from abc import ABC, abstractmethod
+import sys
 
 class Display(ABC):
 
@@ -34,29 +35,39 @@ class Agent:
 
     def start(self, chat, rounds=999999):
         for _ in range(rounds):
-            answer = self.message_claude(chat.to_list(), self.system_prompt)
-            self.display.assistant_says(answer)
-            chat.assistant_says(answer)
-
             try:
-                user_input = self.display.input()
-                if user_input.strip():
-                    chat.user_says(user_input)
-                    continue
+                answer = self.message_claude(chat.to_list(), self.system_prompt)
+                self.display.assistant_says(answer)
+                chat.assistant_says(answer)
+
+                if self._check_for_escape():
+                    user_input = self.display.input()
+                    if user_input.strip():
+                        chat.user_says(user_input)
+                        continue
+
+                parsed_tool = self.tools.parse_tool(answer)
+                if parsed_tool:
+                    self.display.tool_about_to_execute(parsed_tool)
+                    tool_result = self.tools.execute_parsed_tool(parsed_tool)
+                    if parsed_tool.tool_instance.is_completing():
+                        self.save_chat(chat)
+                        self.display.exit()
+                        return tool_result
+                    self.display.tool_result(tool_result)
+                    chat.user_says("Result of " + str(parsed_tool) + "\n" + tool_result)
+
+                self.save_chat(chat)
             except (EOFError, KeyboardInterrupt):
                 self.display.exit()
                 break
-
-            parsed_tool = self.tools.parse_tool(answer)
-            if parsed_tool:
-                self.display.tool_about_to_execute(parsed_tool)
-                tool_result = self.tools.execute_parsed_tool(parsed_tool)
-                if parsed_tool.tool_instance.is_completing():
-                    self.save_chat(chat)
-                    self.display.exit()
-                    return tool_result
-                self.display.tool_result(tool_result)
-                chat.user_says("Result of " + str(parsed_tool) + "\n" + tool_result)
-
-            self.save_chat(chat)
         return None
+
+    def _check_for_escape(self):
+        if sys.platform == "win32":
+            import msvcrt
+            if msvcrt.kbhit():
+                key = msvcrt.getch()
+                if key == b'\x1b':  # ESC key
+                    return True
+        return False
