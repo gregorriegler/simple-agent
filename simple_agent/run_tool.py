67df3288b1d27771ebcd3ddbba7472fc89876ff8
Gregor Riegler <rieglerg85@gmail.com>
2025-10-23 21:24:22 +0200

    r

--- simple_agent/run_tool.py
+++ simple_agent/run_tool.py
@@ -1,17 +1,10 @@
 #!/usr/bin/env python
 import sys
-from simple_agent.application.input import Input
-from simple_agent.infrastructure.console_user_input import ConsoleUserInput
-from simple_agent.infrastructure.stdio import StdIO
 from simple_agent.tools import AllTools
 from simple_agent.tools.subagent_context import SubagentContext
-from simple_agent.tools.subagent_console_display import ConsoleSubagentDisplay
 
 if __name__ == "__main__":
     command = ' '.join(sys.argv[1:])
-    io = StdIO()
-    create_subagent_display = lambda agent_id, indent: ConsoleSubagentDisplay(indent, io)
-    create_subagent_input = lambda indent: Input(ConsoleUserInput(indent, io))
 
     subagent_context = SubagentContext(
         None,
