93397b43214fcf4bfcef1c8eb1d4e4322484b4a5
Gregor Riegler <rieglerg85@gmail.com>
2025-12-19 22:26:29 +0100

    r

--- simple_agent/application/agent.py
+++ simple_agent/application/agent.py
@@ -89,10 +89,6 @@ class Agent:
 
                 tool_result = await self.tools_executor.execute_tools(tools)
 
-                if tool_result.was_cancelled():
-                    self.context.user_says(tool_result.cancelled_message())
-                    raise asyncio.CancelledError()
-
                 if tool_result.has_continue_results():
                     self.context.user_says(tool_result.format_continue_message())
 
